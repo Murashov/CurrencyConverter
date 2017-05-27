ca6aabe9fd4ace4369d877a871010eb433ed4747
Murashov <murashov94@gmail.com>
2017-05-27 21:15:28 +0300

    Changed encoding

--- app/src/main/java/com/artem/currencyconverter/data/remote/RemoteCurrencyLoader.java
+++ app/src/main/java/com/artem/currencyconverter/data/remote/RemoteCurrencyLoader.java
@@ -14,6 +14,7 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class RemoteCurrencyLoader {
         try {
             URL url = new URL(mUrl);
 
-            in = new BufferedReader(new InputStreamReader(url.openStream()));
+            in = new BufferedReader(new InputStreamReader(url.openStream(), "Windows-1251"));
             String str;
             while ((str = in.readLine()) != null) {
                 sb.append(str);
